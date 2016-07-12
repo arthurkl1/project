06403052c517faa063f2f09a058a9b71ff474055
Mac 17 Pouces <Mac17Pouces@MacBook-Pro-de-Mac.local>
2016-07-12 14:23:18 +0200

    ok

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
 
-echo "je suis beau !";
+/*require_once 'vendor/autoload.php';
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=bddland;charset=utf8', 'root', 'root');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+$query = $bdd->query('SELECT * FROM menu');
+$data = $query->fetchAll();
+
+$loader = new Twig_Loader_Filesystem('assets/templates');
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index.twig',
+    array(
+        "menu" => $data
+    )
+);*/
+
 
